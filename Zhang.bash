679e42cadbe300944b909fede426305625d4f1b3
ZhangKunze <Zhang-Kunze@outlook.com>
2025-10-31 12:41:18 +0800

    Add files via upload

---
+++ Zhang.bash
@@ -0,0 +1,16 @@
+#!/data/data/com.termux/files/usr/bin/bash
+echo "当前路径是:"
+sleep 3
+echo $PWD
+sleep 2
+echo "是什么目录自己看，你眼又不瞎"
+sleep 5
+echo "好无聊呀"
+sleep 2
+echo "让我看一下你的权限"
+sleep 4
+whoami
+sleep 1
+echo "怎么权限那么低，我来给你提升一下权限吧"
+sleep 7
+sh rish
